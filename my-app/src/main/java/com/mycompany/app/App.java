b6c3c6d171370a35a0e0cf8ebc68965305946e63
root <root@JenkinsServer.acbu3nn10b4unodmadvr5u4mkd.cx.internal.cloudapp.net>
2021-04-12 11:35:00 +0000

    added msg

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Sreenivasa2!" );
+        System.out.println( "Hello Sreenivasa! Sucessfully built project through Jenkins pipeline" );
     }
 }
